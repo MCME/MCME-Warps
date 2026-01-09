3b39636ba6f2eb6225aac016281911f257747018
Drayz <65258846+Drayz64@users.noreply.github.com>
2026-01-09 23:21:09 +0000

    feat: Added new icons (traffic light + ship wreck)

--- src/main/java/com/mcmiddleearth/warps/core/WarpIcon.java
+++ src/main/java/com/mcmiddleearth/warps/core/WarpIcon.java
@@ -3,7 +3,10 @@ package com.mcmiddleearth.warps.core;
 public enum WarpIcon {
     // Full list: https://github.com/webbukkit/dynmap/wiki/Using-Markers#marker-icons
     DEFAULT("greenflag"),
-    WIP("construction");
+    WIP("construction"),
+    ON_HOLD("yellowflag"),
+    NOT_STARTED("redflag"),
+    PIRATE("pirateflag");
 
     private final String value;
 
