15553c2fd21ba6305d53bedc75d2dc294f038db1
Drayz <65258846+Drayz64@users.noreply.github.com>
2026-01-09 22:48:48 +0000

    feat: assets command (#6)

--- src/main/java/com/mcmiddleearth/warps/velocity/WarpVelocity.java
+++ src/main/java/com/mcmiddleearth/warps/velocity/WarpVelocity.java
@@ -106,6 +106,7 @@ public class WarpVelocity {
             .then(SetIcon.register( WarpRequirements.hasPerm(Permission.SET_ICON) ))
             .then(ListCommand.register( WarpRequirements.hasPerm(Permission.LIST) ))
             .then(PrivateListCommand.register( WarpRequirements.hasPerm(Permission.LIST) ))
+            .then(Assets.register( WarpRequirements.hasPerm(Permission.LIST) ))
             // Commands that require modifiable warps
             .then(Delete.register( WarpRequirements.hasPermAndWarps(Permission.DELETE) ))
             .then(Rename.register( WarpRequirements.hasPermAndWarps(Permission.RENAME) ))
